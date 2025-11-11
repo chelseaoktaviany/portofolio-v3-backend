75dd3692c35eb920dd355acbdffff51311dfd21a
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2025-11-11 20:19:07 +0700

    removed index.ts

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-
-// port
-const port = process.env.PORT || 4000;
-
-// running the server
-app.listen(port, () => {
-  console.log(`server is running on port ${port}`);
-});
